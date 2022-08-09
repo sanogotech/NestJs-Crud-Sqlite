d1bf9673401325df34af85ba954cab8353a08404
SANOGOTECH <sanogotech@yahoo.fr>
2022-08-09 16:49:30 +0000

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { ContactsModule } from './contacts/contacts.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+
 @Module({
   imports: [ContactsModule,       
   TypeOrmModule.forRoot({
